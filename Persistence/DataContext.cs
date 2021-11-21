d9515902c16a1aba4fe21fcf694ebf14a2d9cdf2
linhnguyen26 <linhnlnse140957@fpt.edu.vn>
2021-11-21 23:11:53 +0700

    Adding Clean Architecture with CQRS and Mediator pattern template

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Activity> Activities { get; set; }
+    }
+}
